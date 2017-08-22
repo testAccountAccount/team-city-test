452d1cb752db79474ab8a5d0ee1d26980fce5dc8
Martin Stewart <martin.stewart@tretton37.com>
2017-08-22 10:58:56 +0200

    Removed builder folder and added a nuget package.

--- TeamCityBuild/ProjectToBuild/Program.cs
+++ TeamCityBuild/ProjectToBuild/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace ProjectToBuild
     {
         static void Main(string[] args)
         {
+            var array = new[] { 1, 2, 3 }.ToImmutableArray();
             Console.WriteLine("Works!");
+            Console.WriteLine(array);
             Console.ReadLine();
         }
     }
